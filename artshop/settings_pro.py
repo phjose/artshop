4c35023f445e06774ca71c7e81ad1ce486ff30db
phjose <phjose@gmail.com>
2024-02-13 09:58:58 +0100

    include ddbb pro

--- artshop/settings_pro.py
+++ artshop/settings_pro.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'artshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_pro.sqlite3',
     }
 }
 
